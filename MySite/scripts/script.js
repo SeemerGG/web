92bee542927a256468737d9231f1d11b3a83e49d
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2023-04-05 23:25:06 +0300

    Сделал 3, 4 задание по js (4 может быть перепишу)

--- MySite/scripts/script.js
+++ MySite/scripts/script.js
@@ -8,16 +8,13 @@ function launchButton() {
                     content.style.maxHeight = null;
                 }
                 else {
-                    //content.style.maxHeight =  600 + 'px';
-                    //content.style.height = 600 + 'px';
                     content.style.maxHeight = content.scrollHeight + 'px';
-
                 }
             }
         )
     }
 }
-
+//Task 1
 function launchDataTime() {
     const currentDate = new Date();
     const option = {
@@ -26,13 +23,15 @@ function launchDataTime() {
         second: '2-digit',
         hour12: true
     };
-    // const optionForDate = {
-    //
-    //     weekday: 'long',
-    //     year: 'numeric',
-    //     day: '2-digit',
-    //     month: 'long',
-    // }
+    const days = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+    ];
 
     let dd = currentDate.getDate();
     if (dd < 10) dd = '0' + dd;
@@ -40,17 +39,55 @@ function launchDataTime() {
     let mm = currentDate.getMonth() + 1;
     if (mm < 10) mm = '0' + mm;
 
-    let yy = currentDate.getFullYear() % 100;
+    let yy = currentDate.getFullYear();
     if (yy < 10) yy = '0' + yy;
 
-    let weekDay = currentDate.getDay().
+    let weekDay = days[currentDate.getDay()];
 
-    const finalDate = dd + '.' + mm + '.' + yy;
+    const finalDate = yy + '.' + dd + '.' + mm;
     const finalTime = currentDate.toLocaleTimeString('Ru-ru', option);
-    document.getElementById('dataTime').innerHTML = finalDate + ' ' + finalData;
+    document.getElementById('dataTime').innerHTML = weekDay + ' '  + finalDate + ' ' + finalTime;
 }
+
+//Task 3
+function createArrChildElem() {
+    let mainChildren = document.getElementById('main1').childNodes;
+    return Array.prototype.slice.call(mainChildren);
+}
+
+//Task 4
+function createSquares(count, containerId){
+    let parentContainer = document.getElementById(containerId);
+    for(let i = 0; i < count; i++){
+        let square = document.createElement('div');
+        square.style.height = '100px';
+        square.style.width = '100px';
+        square.style.background = 'lightblue'
+        square.style.justifySelf = 'center';
+        parentContainer.append(square);
+    }
+}
+
+function hideRandomObject(containerId){
+    let parentContainer = document.getElementById(containerId).children;
+    const max = parentContainer.length;
+    const min = 0;
+    const randomNum = Math.floor(Math.random() * (max - min)) + min;
+    parentContainer[randomNum].style.visibility = 'hidden';
+}
+function showAllObject(containerId){
+    let parentContainer = document.getElementById(containerId).children;
+    for(let i = 0; i < parentContainer.length; i++){
+        parentContainer[i].style.visibility = 'visible';
+    }
+}
+
 window.onload = () => {
-    launchButton()
-    launchDataTime()
-    setInterval(launchDataTime, 1000)
+    launchButton();
+    launchDataTime();
+    setInterval(launchDataTime, 1000);
+    document.getElementById('countChildEl').innerText = createArrChildElem().length.toString();
+    createSquares(4, 'editObjects');
+    setInterval(() => {hideRandomObject('editObjects')}, 120);
+    setInterval(() => {showAllObject('editObjects')}, 240);
 }
