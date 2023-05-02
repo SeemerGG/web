549b773dd870d98cd99bf236fd2d84125af4cbe3
Seemer <92618851+SeemerGG@users.noreply.github.com>
2023-05-02 14:04:38 +0300

    Календарь

--- MySite/scripts/script.js
+++ MySite/scripts/script.js
@@ -82,6 +82,47 @@ function showAllObject(containerId){
     }
 }
 
+//Task 2
+function calendar(id, year, month) {
+    var Dlast = new Date(year, month + 1, 0).getDate(),
+        D = new Date(year, month, Dlast),
+        DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
+        DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+        calendar = '<tr>',
+        month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    if (DNfirst != 0) {
+        for (var i = 1; i < DNfirst; i++) calendar += '<td>';
+    } else {
+        for (var i = 0; i < 6; i++) calendar += '<td>';
+    }
+    for (var i = 1; i <= Dlast; i++) {
+        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+            calendar += '<td class="today">' + i;
+        } else {
+            calendar += '<td>' + i;
+        }
+        if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+            calendar += '<tr>';
+        }
+    }
+    for (var i = DNlast; i < 7; i++) calendar += '<td> ';
+    document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+    document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
+    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
+    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+    if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {
+        document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td> <td> <td> <td> <td> <td> <td> ';
+    }
+}
+
+//Task 5
+
+function createLi(){
+    const  str = prompt('Имя пункта:').trim();
+    const list = document.getElementById('list');
+    list.insertAdjacentHTML('beforeend', '<li></li>')
+}
+
 window.onload = () => {
     launchButton();
     launchDataTime();
@@ -90,4 +131,20 @@ window.onload = () => {
     createSquares(4, 'editObjects');
     setInterval(() => {hideRandomObject('editObjects')}, 120);
     setInterval(() => {showAllObject('editObjects')}, 240);
+
+    calendar("calendar", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+    document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+        calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) - 1);
+    }
+// переключатель плюс месяц
+    document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+        calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) + 1);
+    }
+
+    //В разработке
+    document.getElementById('addEll').onclick = createLi;
+    //
+
+
 }
